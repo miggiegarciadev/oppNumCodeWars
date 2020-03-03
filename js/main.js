f886055e0dce76bcb036e7b7c77bd981f4d3258e
Miggie Garcia <60056056+miggiegarcia@users.noreply.github.com>
2020-03-02 21:15:06 -0500

    daily code war challenge

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ // Very simple, given a number, find its opposite.
+// PREP
+// Parameters num, -num
+//Results num * -1 = -num || -num *-1 = num
+//Example num = 7 opposite = -7 || num = -10 opposite = 10
+//Psuedo Code In order to get a the opposite of a ositive number
+// mutiple it by negative one and if you want the opposite of a
+// negative number multiple it by negative one
+
+ function opposite(number) {
+ return number*-1;
+ }
